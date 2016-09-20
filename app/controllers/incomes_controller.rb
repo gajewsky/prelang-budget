18d379d9f02027c4dc7e0b801553f8c638892669
gajewsky <marcin.gajewski@rebased.pl>
2016-09-20 11:02:06 +0200

    clean up controllers

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,75 +1,52 @@
 class IncomesController < ApplicationController
+  before_filter :require_user_signed_in
   before_action :set_income, only: [:show, :edit, :update, :destroy]
 
-  # GET /incomes
-  # GET /incomes.json
   def index
     @incomes = Income.all
   end
 
-  # GET /incomes/1
-  # GET /incomes/1.json
   def show
   end
 
-  # GET /incomes/new
   def new
     @income = Income.new
   end
 
-  # GET /incomes/1/edit
   def edit
   end
 
-  # POST /incomes
-  # POST /incomes.json
   def create
     @income = Income.new(income_params)
     @income.user = current_user
-
-    respond_to do |format|
-      if @income.save
-        format.html { redirect_to @income, notice: 'Income was successfully created.' }
-        format.json { render :show, status: :created, location: @income }
-      else
-        format.html { render :new }
-        format.json { render json: @income.errors, status: :unprocessable_entity }
-      end
+    if @income.save
+      redirect_to incomes_url, notice: 'Income was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /incomes/1
-  # PATCH/PUT /incomes/1.json
   def update
-    respond_to do |format|
-      if @income.update(income_params)
-        format.html { redirect_to @income, notice: 'Income was successfully updated.' }
-        format.json { render :show, status: :ok, location: @income }
-      else
-        format.html { render :edit }
-        format.json { render json: @income.errors, status: :unprocessable_entity }
-      end
+    if @income.update(income_params)
+      redirect_to incomes_url, notice: 'Income was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /incomes/1
-  # DELETE /incomes/1.json
   def destroy
     @income.destroy
-    respond_to do |format|
-      format.html { redirect_to incomes_url, notice: 'Income was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to incomes_url, notice: 'Income was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_income
-      @income = Income.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_income
+    @income = Income.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def income_params
-      params.require(:income).permit(:value, :earn_date, :subcategory_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def income_params
+    params.require(:income).permit(:value, :earn_date, :subcategory_id)
+  end
 end
