afd4852cc707eb8385ee7847c68d4f44a2f21872
gajewsky <marcin.gajewski@rebased.pl>
2016-09-28 20:56:22 +0200

    dividable

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -8,6 +8,8 @@ class Expense < ActiveRecord::Base
   validates :value, presence: true
   validates :subcategory, presence: true
 
+  scope :dividable, -> { where(to_divide: true) }
+
   def self.group_by_subcategory(expenses = Expense.all)
     expenses.group_by(&:subcategory).map do |subcategory, subexpenses|
       [subcategory&.label, subexpenses.map(&:value).reduce(:+)]
