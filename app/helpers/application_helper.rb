16ba509a086344311b5784d532f11ab4378e97fd
gajewsky <marcin.gajewski@rebased.pl>
2016-10-03 23:16:04 +0200

    Tables and forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 # Bunch of view helper methods used both in incomes and expenses
-module AmountsHelper
+module ApplicationHelper
   def sum_values(valuable)
     return 0 if valuable.empty?
     valuable.map(&:value).reduce(:+)
@@ -7,5 +7,14 @@ module AmountsHelper
 
   def number_to_pln(number)
     ActionController::Base.helpers.number_to_currency(number, unit: 'zł', format: '%n %u')
+  end
+
+  def flash_class(level)
+    case level
+      when 'notice' then 'alert alert-info'
+      when 'success' then 'alert alert-success'
+      when 'error' then 'alert alert-error'
+      when 'alert' then 'alert alert-error'
+    end
   end
 end
