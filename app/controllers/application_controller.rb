e7d774b394e8aca6316ac3a21561f5442406b238
gajewsky <marcin.gajewski@rebased.pl>
2017-01-16 20:51:32 +0100

    Add user avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # A single point of entry for all the customizations and extensions needed for
 # an action controller
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -12,5 +13,12 @@ class ApplicationController < ActionController::Base
   def require_user_signed_in
     return if user_signed_in?
     redirect_to new_user_session_path, flash: { error: 'You must be signed in to view this page.' }
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
   end
 end
