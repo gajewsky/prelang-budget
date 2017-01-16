06ea3e1a1a86d3a6c365aa81a65176c7a5870c26
gajewsky <marcin.gajewski@rebased.pl>
2017-01-16 21:17:39 +0100

    Cap fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,8 @@ set :repo_url, 'git@github.com:gajewsky/prelang-budget.git'
 set :deploy_to, '/home/deploy/budget'
 
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :bundle_binstubs, nil
 
 namespace :deploy do
 
