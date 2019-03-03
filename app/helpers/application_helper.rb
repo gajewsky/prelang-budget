f598055e198ae97c54684ceaae111c49b6bed950
Marcin Gajewski <code@gajewsky.it>
2019-03-03 16:43:20 +0100

    Setup circle ci

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,7 @@ module ApplicationHelper
 
   def sum_values(valuable)
     return 0 if valuable.empty?
+
     valuable.map(&:value).reduce(:+)
   end
 
@@ -13,13 +14,16 @@ module ApplicationHelper
 
   def pln_per_month(number)
     return 0 unless number
+
     today = Date.today
-    result = number.to_f / ((today.year * 12 + today.month) - (BUDGET_START_DATE.year * 12 + BUDGET_START_DATE.month))
+    months_since_start = BUDGET_START_DATE.year * 12 + BUDGET_START_DATE.month
+    result = number.to_f / ((today.year * 12 + today.month) - months_since_start)
     ActionController::Base.helpers.number_to_currency(result, unit: 'zł', format: '%n %u')
   end
 
   def pln_per_day(number)
     return 0 unless number
+
     result = number / (Date.today - BUDGET_START_DATE).to_f
 
     ActionController::Base.helpers.number_to_currency(result, unit: 'zł', format: '%n %u')
