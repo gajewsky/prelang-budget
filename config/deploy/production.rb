f45132a82cc146a5535bb129e15f3fc803d0e6b8
gajewsky <code@gajewsky.it>
2019-08-31 13:54:08 +0000

    Bugfix/capistrano (#17)
    * Capistrano example setup
    
    * Setup capistrano proper way

--- config/deploy/production.rb
+++
@@ -1,5 +0,0 @@
-set :stage, :production
-server_ip = Rails.application.credentials.capistrano.dig(:production, :server)
-user = Rails.application.credentials.capistrano.dig(:production, :user)
-
-server server_ip, user: user, roles: %w{web db app}
