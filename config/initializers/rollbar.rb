29f55b1c96cf293c34eec0363f718ccde51c413f
gajewsky <code@gajewsky.it>
2019-09-06 13:19:59 +0000

    Improvement/create bill (#24)
    * Prepare factories
    
    * Specs for bill create
    
    * CreateBill service

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,7 +1,7 @@
 Rollbar.configure do |config|
   config.access_token = Rails.application.credentials.rollbar[:access_token]
 
-  config.enabled = false if Rails.env.test?
+  config.enabled = false if Rails.env.test? || Rails.env.develop?
 
   config.environment = ENV['ROLLBAR_ENV'].presence || Rails.env
 end
